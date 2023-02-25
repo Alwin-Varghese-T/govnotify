f476b5c0cfd571cb1a98fdcae37a8785e4a5c9de
Alwin-Varghese <90436500+Alwin-Varghese-T@users.noreply.github.com>
2023-02-25 12:07:49 +0000

    add db string to secrets

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connect_string = "mysql+pymysql://cc368utl7dt898dkdz23:pscale_pw_rHmdaRZZR219HzQ8IFSutYoOqt5HrlUXdn7SX9go8oC@ap-south.connect.psdb.cloud/govnotify?charset=utf8mb4"
+db_connect_string = os.environ['db_connect_string']
 
 engine = create_engine(db_connect_string,
                        connect_args={"ssl": {
