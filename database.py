fdf26373a02f856354c0cb0ad60fb98af06d38d4
Alwin-Varghese <90436500+Alwin-Varghese-T@users.noreply.github.com>
2023-02-25 12:35:03 +0000

    asd

--- database.py
+++ database.py
@@ -1,16 +1,18 @@
 from sqlalchemy import create_engine, text
-import os
+from os import environ
 
-db_connect_string = os.environ['db_connect_string']
+db_connect_string = environ["db_connect"]
 
 engine = create_engine(db_connect_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
+
+
 def load_links():
   with engine.connect() as conn:
     result = conn.execute(text("select * from sample"))
     items = []
     for row in result:
       items.append(row._asdict())
-    return items  
+    return items
